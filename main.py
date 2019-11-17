dee4b74eca598c5de73fe76151d952482d4bfc77
gray <shinjoohyung25@gmail.com>
2019-11-17 02:35:10 -0500

    fix main with data load

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import numpy as np
+import pandas as pd
 import data_loader as dl
 
+
+train_data = dl.load_clean_data('train')
+x_train, y_train = (train_data[0], train_data[-1])
+test_data = dl.load_clean_data('test')
+x_test, y_test = (test_data[0], test_data[-1])
