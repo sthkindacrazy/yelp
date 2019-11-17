a4eacb969146f67b237048948a781f7c65dcb484
gray <shinjoohyung25@gmail.com>
2019-11-17 02:26:08 -0500

    add clean data

--- data_loader.py
+++ data_loader.py
@@ -1,14 +1,24 @@
 import os
 import numpy as np
 import pandas as pd
+import cleaning as cl
+
 
 basedir = os.path.dirname(os.path.realpath(__file__))
 data_path = os.path.join(basedir, 'data')
 
 
 # load train
-def load_data(dt='train'):
+def load_raw_data(dt='train'):
     data = pd.read_csv(data_path + '/Yelp_' + dt + '.csv')
+    x_data = data.iloc[:, 1:]
+    y_data = data.iloc[:, 0]  # stars
+    return x_data, y_data
+
+
+def load_clean_data(dt='train'):
+    data = pd.read_csv(data_path + '/Yelp_' + dt + '.csv')
+    data = cl.cleaning(data)
     x_data = data.iloc[:, 1:]
     y_data = data.iloc[:, 0]  # stars
     return x_data, y_data
