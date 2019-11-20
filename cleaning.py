cea301e8c5c0a04ede7b6d4006f6ef4e1fbf7811
gray <shinjoohyung25@gmail.com>
2019-11-20 00:27:36 -0500

    add tf-idf

--- cleaning.py
+++ cleaning.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.stem.snowball import SnowballStemmer
 from tqdm import tqdm
 from sklearn.feature_extraction.text import CountVectorizer
-
+from sklearn.feature_extraction.text import TfidfTransformer
 stemmer = SnowballStemmer('english')
 
 
@@ -37,3 +37,15 @@ def phrase_one_hot_encode(data, num=30000):
     vectorizer = CountVectorizer(max_features=num, min_df=1, ngram_range=(1, 2))
     vectorizer.fit(data['text'])
     return vectorizer.transform(data['text'])
+
+
+def phrase_tf_idf_encode(data, num=30000):
+    vectorizer = CountVectorizer(max_features=num, min_df=1, ngram_range=(1, 2))
+    transformer = TfidfTransformer(smooth_idf=False)
+    vectorizer.fit(data['text'])
+    counts = vectorizer.transform(data['text'])
+    tfidf = transformer.fit_transform(counts)
+    return tfidf
+
+
+
