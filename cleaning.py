5bdf4323859f53f8415a28ee7fd40ec0bd744d2d
guannant <57839515+guannant@users.noreply.github.com>
2019-11-22 19:12:33 -0500

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -3,6 +3,10 @@ from nltk.stem.snowball import SnowballStemmer
 from tqdm import tqdm
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+
+stop_words = set(stopwords.words('english'))
 stemmer = SnowballStemmer('english')
 
 
@@ -14,7 +18,6 @@ def stem_phrase(phrase):
         stemmed_words.append(stemmed_word)
     stemmed_phrase = " ".join(stemmed_words)
     return stemmed_phrase
-
 
 # you can add if you find some additional fix or cleaning
 def clean_text(phrase):
@@ -46,6 +49,28 @@ def phrase_tf_idf_encode(data, num=30000):
     counts = vectorizer.transform(data['text'])
     tfidf = transformer.fit_transform(counts)
     return tfidf
+
+###revised part
+def cln(data):
+    cln_text =[]
+    for sentence in data["text"]:
+        cln_text.append(re.sub("[!&\'()*+,-./:;<=>?@[\\]^_`{|}~]", "", sentence))
+    data["cln_text"] = cln_text
+    return data
+
+def rm_stopwords(data):
+    data_cl = cln(data)
+    filtered_sentence=[]
+    for sentence in data_cl["cln_text"]:
+        word_tokens = word_tokenize(sentence) 
+        sentence_tem = [w for w in word_tokens if not w in stop_words]
+        filtered_sentence.append(sentence_tem)
+    data["cln_split_text"] = filtered_sentence
+    clc_sentence = []
+    for sentence in data["cln_split_text"]:
+        clc_sentence.append(" ".join(sentence))
+    data["cln_split_text"]=clc_sentence
+    return data
 
 
 
